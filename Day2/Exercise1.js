33e875d7bf10b30dc56c7a53ef64e20bbe2227cd
surabhisharma0708 <surabhi.sharma_cs19@gla.ac.in>
2022-05-01 12:16:53 +0530

    Spliting a string into an array

--- Day2/Exercise1.js
+++ Day2/Exercise1.js
@@ -9,3 +9,10 @@ console.log(challenge.toUpperCase);
 //5
 console.log(challenge.toLowerCase);
 //6
+console.log(challenge.substring(0,2));
+//7
+console.log(challenge.substring(3,challenge.length));
+//8
+console.log(challenge.includes('Script'));
+//9
+console.log(challenge.split());
