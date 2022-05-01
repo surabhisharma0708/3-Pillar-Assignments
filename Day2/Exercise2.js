d0c0d3ebc25b3f1ee5d7292979f88d04060daee0
surabhisharma0708 <surabhi.sharma_cs19@gla.ac.in>
2022-05-01 13:20:28 +0530

    Checking for equality

--- Day2/Exercise2.js
+++ Day2/Exercise2.js
@@ -3,3 +3,16 @@ console.log('The quote \'There is no exercise better for the heart than reaching
 //2
 console.log("Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.");
 //3
+let a ='10';
+let b = 10;
+if(a!==b)
+{
+    a = Number(a);
+    console.log('Now , a and b are equal');
+}
+else
+{
+    console.log('a and b are already equal');
+}
+
+//
