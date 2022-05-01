87553cbc7519ec015cc6f037719ffb1b32f208fe
surabhisharma0708 <surabhi.sharma_cs19@gla.ac.in>
2022-05-01 14:32:11 +0530

    slicing using substr method

--- Day2/Exercise2.js
+++ Day2/Exercise2.js
@@ -52,3 +52,7 @@ console.log(j.charAt(x));
 //11
 console.log('1 1 1 1 \n2 1 2 4 8 \n3 1 3 9 27 \n4 1 4 16 64 \n5 1 5 25 125');
 //12
+let t = 'You cannot end a sentence with because because because is a conjunction';
+console.log(t.substr(31,23));
+
+
