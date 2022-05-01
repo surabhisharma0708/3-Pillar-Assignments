9da1fec28b69ee884ceaca7c933dd7234a7e421a
surabhisharma0708 <surabhi.sharma_cs19@gla.ac.in>
2022-05-01 12:09:58 +0530

    Changing the string to defined cases

--- Day2/Exercise1.js
+++ Day2/Exercise1.js
@@ -4,3 +4,8 @@ let challenge = '30 Days Of Javascript';
 console.log(challenge);
 //3
 console.log(challenge.length);
+//4
+console.log(challenge.toUpperCase);
+//5
+console.log(challenge.toLowerCase);
+//6
