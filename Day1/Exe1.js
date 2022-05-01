4615d4b3f280951a68d3f19552f38f7aae818020
surabhisharma0708 <surabhi.sharma_cs19@gla.ac.in>
2022-05-02 00:11:49 +0530

    Declaration and intialization of variables

---
+++ Day1/Exe1.js
@@ -0,0 +1,37 @@
+//1.
+//comments can make code readable
+//2.
+// Welcome to 30DaysOfJavaScript
+//3.
+/* comments can make code readable,
+  easy to reuse and informative*/
+//4.
+let str = 'I can do this.';
+let bool = true;
+let undef ;
+let nu = null;
+//5.
+console.log(typeof(str));
+console.log(typeof(bool));
+console.log(typeof(undef));
+console.log(typeof(nu));
+//6.
+let a,b,c,d;
+//7.
+let w = 1;
+let x = 'Surabhi';
+let y = ['abc','xyz',10,20];
+let z = {name: 'Surabhi',Age:25,City:'Narora'};
+//8.
+let fname = 'Surabhi';
+let lname = 'Sharma';
+let marital = 'Single';
+let country ='India';
+let age = 25;
+//9.
+let f_name = 'Surabhi',l_name = 'Sharma',marital_sts = 'Single',country_ ='India',age_ = 25;
+//10.
+let myAge = 10;
+let yourAge = 20;
+console.log(myAge);
+console.log(yourAge);
