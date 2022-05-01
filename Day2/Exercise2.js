236168287f48d2990bbdf20e47610f45dac78d14
surabhisharma0708 <surabhi.sharma_cs19@gla.ac.in>
2022-05-01 13:15:06 +0530

    commit

---
+++ Day2/Exercise2.js
@@ -0,0 +1,5 @@
+//1
+console.log('The quote \'There is no exercise better for the heart than reaching down and lifting people up.\' by John Holmes teaches us to help one another.');
+//2
+console.log("Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.");
+//3
