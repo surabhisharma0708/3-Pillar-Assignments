41311a24f0310370d8c774255f38db31fce53702
surabhisharma0708 <surabhi.sharma_cs19@gla.ac.in>
2022-05-02 10:29:06 +0530

    Calulating the area of various shapes

---
+++ Day3/Level2.js
@@ -0,0 +1,41 @@
+//1.
+let a = prompt('Enter the base Value');
+let b = prompt('Enter the height value');
+let area = 0.5*a*b;
+console.log("The area of triangle is"+area);
+//2.
+let side_a =prompt('Enter the side 1');
+let  side_b =prompt('Enter the side 2');
+let side_c =prompt('Enter the side 3');
+
+let peri=side_a+side_b+side_c;
+console.log(peri);
+
+//3.
+let len =prompt('Enter the length');
+let wid =prompt('Enter the width');
+console.log(2*(len+wid));
+
+//4.
+let rad = prompt('Enter the radius');
+let pi = 3.14;
+let cir_area = pi*r*r;
+let circum = 2*pi*r;
+console.log(air_area);
+console.log(circum);
+
+//5.
+// y=2x-2;
+y=mx+c;
+
+let slop1 = ;
+//6.
+let x1=2;
+let x2=6;
+let y1=2;
+let y2=10;
+let slop2 = (y2-y1)/(x2-x1);
+console.log(slop);
+
+//7.
+
