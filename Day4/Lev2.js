a8571a62fdc35b7970f5eeb03a971c506fc9b364
surabhisharma0708 <surabhi.sharma_cs19@gla.ac.in>
2022-05-04 02:34:57 +0530

    Checking for the seasons

--- Day4/Lev2.js
+++ Day4/Lev2.js
@@ -21,3 +21,21 @@ else if(scores>=0 && scores<=49)
     console.log("F");
 }
 
+//2
+let season = prompt("Enter season");
+if(season==="September" ||season==="October" || season==="November")
+{
+    console.log("The season is Autumn");
+}
+else if(season==="December" ||season==="January" || season==="February")
+{
+    console.log("The season is Winter");
+}
+else if(season==="March" ||season==="April" || season==="May")
+{
+    console.log("The season is Spring");
+}
+else if(season==="June" ||season==="July" || season==="August")
+{
+    console.log("The season is Summer");
+}
