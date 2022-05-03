e6655637df6c2b0b40a763ab649e99683511f806
surabhisharma0708 <surabhi.sharma_cs19@gla.ac.in>
2022-05-04 02:29:32 +0530

    Grades of students according to their marks

---
+++ Day4/Lev2.js
@@ -0,0 +1,23 @@
+//1.
+let scores = prompt("Enter scores");
+if(scores>=80 && scores<=100)
+{
+    console.log("A");
+}
+else if(scores>=70 && scores<=79)
+{
+    console.log("B");
+}
+else if(scores>=60 && scores<=69)
+{
+    console.log("C");
+}
+else if(scores>=50 && scores<=59)
+{
+    console.log("D");
+}
+else if(scores>=0 && scores<=49)
+{
+    console.log("F");
+}
+
