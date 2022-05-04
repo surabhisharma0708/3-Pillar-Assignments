ceb7dd497dfee429808e231048d08001e887b949
surabhisharma0708 <surabhi.sharma_cs19@gla.ac.in>
2022-05-04 14:19:43 +0530

    Declaring , initializing and performing operations on it

---
+++ Day5/Lev1.js
@@ -0,0 +1,52 @@
+const countries =[
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya'
+  ];
+const webTechs = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Redux',
+    'Node',
+    'MongoDB'
+  ];
+
+//1
+const ar =[];
+//2
+const ar2 = ["owl","peacock","cock","sparrow","flamingo","parrot"];
+//3
+console.log(ar2.length);
+//4
+console.log(ar2[0]);
+console.log(ar2[ar2.length/2]);
+console.log(ar2[ar2.length-1]);
+
+//5
+
+let mixedDataTypes =["name",1,2,[4,5,6],{name:"surabhi,age:19"}, "class"];
+//6
+let companies = ["Facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle" ,"Amazon"];
+//7
+console.log(companies);
+//8
+console/length(companies.length);
+//9
+console.log(companies[0]);
+console.log(companies[Number((companies.length)/2)]);
+console.log(comapnies[ar2.length-1]);
+//10
+for(let company of companies)
+{
+    console.log(company);
+}
