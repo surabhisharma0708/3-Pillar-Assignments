f394c551facf9cb2a374ae32b121414434e28a95
surabhisharma0708 <surabhi.sharma_cs19@gla.ac.in>
2022-05-08 23:01:01 +0530

    generating a hexadecimal number

--- Day6/Level2.js
+++ Day6/Level2.js
@@ -1,9 +1,22 @@
+//1
+let max_size =10;
 var possible ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 var txt="";
-var len =prompt("Enter the length");
+var len =Math.random()*(max_size-1)+1;
 for(c=0;c<len;c++)
 {
     txt=txt+ possible.charAt(Math.floor(Math.random()*possible.length));
 }
 
 console.log(txt);
+//2
+
+let hexChar = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
+let len1 = Math.random()*(max_size-1)+1;
+let hex_txt="";
+for(c=0;c<len;c++)
+{
+    hex_txt += hexChar[Math.floor(Math.random()*16)];
+
+}
+console.log(hex_txt);
