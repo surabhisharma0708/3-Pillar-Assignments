9d69e3ed080d60ba1c0edc5935bca258d633fda7
surabhisharma0708 <surabhi.sharma_cs19@gla.ac.in>
2022-05-11 21:03:21 +0530

    creation of an object

---
+++ Day8/Level3.js
@@ -0,0 +1,18 @@
+//1
+let personAccount = {
+    firstName: "Surabhi",
+    lastName : "Sharma",
+    income : {
+        totalIncome:100,
+        addIncome:50,
+        accountBalace:"150"
+    },
+    expenses : {
+        
+        totalExpenses:200,
+        accountInfo:"SBI",
+        
+        addExpense:"50",
+        
+    }
+}
