282b9f93c274c8924b8bd01cdbadeda9c07c374d
surabhisharma0708 <surabhi.sharma_cs19@gla.ac.in>
2022-05-12 12:26:40 +0530

    Difference between forEach, map,filter and reduce

---
+++ Day9/Level1.js
@@ -0,0 +1,16 @@
+//1
+/* 
+forEach : It is only used to iterate array elements.It takes a callback function with elements, index parameter and array itself.
+map : It iterate an array elements and modify the array elements.
+filter: It filter out items which full fill filtering conditions and return a new array.
+reduce: Reduce takes a callback function. The call back function takes accumulator, current, and optional initial value as a parameter and returns a single value. It is a good practice to define an initial value for the accumulator value. If we do not specify this parameter, by default accumulator will get array first value.
+*/
+//2
+//3
+//4
+//5
+//6
+//7
+//8
+//9
+//10
--- practice.js
+++ practice.js
@@ -1,19 +1,80 @@
-const cats =['Leopard', 'Serval', 'Jaguar', 'Tiger', 'Caracal', 'Lion'];
+// const cats =['Leopard', 'Serval', 'Jaguar', 'Tiger', 'Caracal', 'Lion'];
 
-function toUpper(str)
-{
-    return str.toUpperCase();
-}
-const upperCats =cats.map(toUpper);
-console.log(upperCats);
+// function toUpper(str)
+// {
+//     return str.toUpperCase();
+// }
+// const upperCats =cats.map(toUpper);
+// console.log(upperCats);
 
-function lcat(st)
-{
-    return st.startsWith('L');
-}
-const filtered = cats.filter(lcat);
-console.log(filtered);
+// function lcat(st)
+// {
+//     return st.startsWith('L');
+// }
+// const filtered = cats.filter(lcat);
+// console.log(filtered);
 
-let original =[1,2,3];
-let doubled = original.map(item=>tem*2);
-console.log(doubled);
+// let original =[1,2,3];
+// let doubled = original.map(item=>tem*2);
+// console.log(doubled);
+
+
+// const callback = (n) =>{
+//     return n**2;
+// } 
+
+// function cube(callback,n)
+// {
+//     return callback(n)*n;
+
+// }
+// console.log(cube(callback,3));
+
+// //returning
+// const higherOrder = n=>{
+//     const doSomething =(m)=>{
+//         const doWhatEver =(t)=>{
+//             return n+m+t;
+//         }
+//         return doWhatEver;
+//     }
+//     return doSomething;
+// }
+// console.log(higherOrder(5)(4)(7));
+
+// //
+// arr = [1,2,3,4,5]
+// arr.forEach(element => {
+//     console.log(element);
+// });
+
+// const countries =['Finland', 'Denmark', 'Sweden', 'Norway', 'Iceland'];
+// countries.forEach((element)=> {
+//     console.log(element.toUpperCase);
+// })
+// //map
+// const newnum = arr.map((element)=> element*element);
+
+// //every
+// const areAllString =countries.every((name)=> typeof name ==='string');
+// console.log(areAllString);
+//find
+const ages = [10,20,30,12,5,63];
+const age = ages.find((ele)=>ele<20)
+console.log(age);
+
+const scores = [
+    { name: 'Asabeneh', score: 95 },
+    { name: 'Mathias', score: 80 },
+    { name: 'Elias', score: 50 },
+    { name: 'Martha', score: 85 },
+    { name: 'John', score: 100 },
+  ];
+  const score = scores.find((user)=>user.score>80)
+  console.log(score);
+
+
+const lessage = ages.findIndex((age)=>age>30)
+console.log(lessage);
+
+//some
