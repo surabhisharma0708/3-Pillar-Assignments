c49816bdfc94de3c2b3096d0a5964dc61045e0d2
surabhisharma0708 <surabhi.sharma_cs19@gla.ac.in>
2022-05-12 21:29:59 +0530

    Destructuring and assigning the elements

---
+++ Day11/Level1.js
@@ -0,0 +1,20 @@
+const scientific = [2.72,3.14,9.18,37,100];
+const [e,pi,gravity,humanBodyTemp,waterBoilingTemp] = scientific;
+console.log(e,pi,gravity,humanBodyTemp,waterBoilingTemp);
+
+//2
+const countries = ['finland','estonia','sweden','denmark','norway'];
+const [fin,est,sw,den,nor] = countries;
+console.log(fin,est,sw,den,nor);
+
+//3
+const rectangle = {
+    width: 20,
+    height: 10,
+    area: 200,
+    perimeter: 60
+}
+let {width: w,height: h,area: a,perimeter: p}= rectangle;
+console.log(w,h,a,p);
+
+
