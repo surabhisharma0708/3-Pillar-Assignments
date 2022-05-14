0b98e2aa28a4a5efa472d8ba5e83bca0fd5ecf6e
surabhisharma0708 <surabhi.sharma_cs19@gla.ac.in>
2022-05-14 19:10:35 +0530

    overriding the method

--- Day15/Level1.js
+++ Day15/Level1.js
@@ -23,5 +23,16 @@ class CAT extends Animal
 }
 class DOG extends Animal
 {
-
+    constructor(name,age,color,legs,gender)
+    {
+        super(name,age,color,legs)
+        this.gender=gender;
+    }
+    getinfo()
+    {
+        let info = `My name is ${this.name}.I am ${this.age} years old.I am of ${this.color} color.I have ${this.legs} legs`;
+        return info;
+    }
 }
+const dog= new DOG('Anmol',18,'black',4,'female');
+console.log(dog.getinfo());
