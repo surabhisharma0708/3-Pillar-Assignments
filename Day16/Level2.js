9f042c81a8ea9fb3993c76cf3296202046541267
surabhisharma0708 <surabhi.sharma_cs19@gla.ac.in>
2022-05-15 23:16:18 +0530

    converting to JSON format using filter

---
+++ Day16/Level2.js
@@ -0,0 +1,9 @@
+const student = {
+    firstName:'Asabeneh',
+    lastName:'Yetayehe',
+    age:250,
+    isMarried:true,
+    skills:['HTML', 'CSS', 'JS', 'React','Node', 'Python', ]
+}
+const st= JSON.stringify(student,['firstName','lastName','skills'],4);
+console.log(st);
